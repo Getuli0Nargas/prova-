219eb3ce053fe89068d5565b51200687e65ff5ff
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-29 16:05:30 -0300

    tentativa de dar certo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,73 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios  from 'axios'
 import './App.css'
-import 
 
 function App() {
+
+  /*Funções UseState*/
   const [marca, setMarca] = useState("")
   const [modelo, setModelo] = useState("")
-  const [ano, setAno] = useState("")
+  const [ano, setAno] = useState(0)
   const [proprietario, setProprietario] = useState("")
-  const [cor, setCor] = useState("")
+  const [cor, setCor] = useState("") 
 
+
+  /* Função anônima */
   useEffect(() => {
-    console.log(marca, modelo, ano, proprietario, cor)
-  }, [marca, modelo, ano, proprietario,cor])
+    console.log(marca, modelo, ano, cor, proprietario)
+  }, [marca, modelo, ano, cor , proprietario])
+
+
+  // npm i axios
+  async function RegisterNewVehicle(){
+    await axios.post("http://localhost:3000/cadastrar",{
+      marca, modelo, ano, cor, proprietario
+
+    })
+  }
+
+  async function UpdateVehicle(){
+    await axios.update("http://localhost:3000/update")
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    RegisterNewVehicle();
+  } //no botão de registrar do form, usar essa função
+  //onclick="() => {handleSubmit}" 
+
 
   return (
     <>
-    <div className='Card'>
-     <form>
-      <label htmlFor="marca">Marca</label>
-      <br />
+      <div className="card">
+        <form onSubmit={() => {RegisterNewVehicle}}>
 
-      <label htmlFor="modelo">Modelo</label>
-      <input type="text" id='modelo'/>
-      <br />
-      <label htmlFor="ano">Ano</label>
-      <input type="text" id='ano'/>
-      <br />
-      <label htmlFor="proprietario">Proprietario</label>
-      <input type="text" id='proprietario'/>
-      <br />
-      <label htmlFor="cor">Cor</label>
-      <input type="text" id='cor'/>
-      <br />
+          <label htmlFor="proprietario">Proprietario</label>
+          <br />
+          <input type="text" id='proprietario'  onChange={(e) => {setProprietario(e.target.value)}} />
+          <br />
+          <label htmlFor="marca">Marca</label>
+          <br />
+          <input type="text" id='marca' onChange={(e) => {setMarca(e.target.value)}}/>
+          <br />
+          <label htmlFor="modelo">Modelo</label>
+          <br />
+          <input type="text" id='modelo'   onChange={(e) => {setModelo(e.target.value)}}/>
+          <br />
+          <label htmlFor="cor">Cor</label>
+          <br />
+          <input type="text" id='cor'  onChange={(e) => {setCor(e.target.value)}}/>
+          <br />
+          <label htmlFor="ano">Ano</label>
+          <br />
+          <input type="number" id='ano'  onChange={(e) => {Number(setAno(e.target.value))}}/>
+          <br />
+          <br />
+          <button type='submit' onClick={handleSubmit}>Registrar veículo</button>
 
 
-     </form>
-    </div>
+        </form>
+      </div>
     </>
   )
 }
